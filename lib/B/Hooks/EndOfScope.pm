09fda95bee93b4479dccc02ef2475dec23fdf8b7
Florian Ragwitz <rafl@debian.org>
2008-10-19 09:26:47 +0200

    Add some docs.

--- lib/B/Hooks/EndOfScope.pm
+++ lib/B/Hooks/EndOfScope.pm
@@ -14,11 +14,55 @@ use Sub::Exporter -setup => {
     groups  => { default => ['on_scope_end'] },
 };
 
+=head1 NAME
+
+B::Hooks::EndOfScope - Execute code after a scope finished compilation
+
+=head1 SYNOPSIS
+
+    on_scope_end { ...  };
+
+=head1 DESCRIPTION
+
+This module allows you to execute code when perl finished compiling the
+surrounding scope.
+
+=head1 FUNCTIONS
+
+=head2 on_scope_end
+
+    on_scope_end { ... };
+
+    on_scope_end $code;
+
+Registers C<$code> to be executed after the surrounding scope has been
+compiled.
+
+=cut
+
 sub on_scope_end (&) {
     my $cb = shift;
 
     $^H |= 0x120000;
     $^H{ $SCOPE_HOOK_KEY } = [Scope::Guard->new($cb), @{ $^H{ $SCOPE_HOOK_KEY } || [] }];
 }
+
+=head1 SEE ALSO
+
+L<Scope::Guard>
+
+=head1 AUTHOR
+
+Florian Ragwitz E<lt>rafl@debian.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2008  Florian Ragwitz
+
+This module is free software.
+
+You may distribute this code under the same terms as Perl itself.
+
+=cut
 
 1;
