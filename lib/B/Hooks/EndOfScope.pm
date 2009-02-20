d2946a2caf0c2c8bb6fb8200f2c1038c0439e3a4
Florian Ragwitz <rafl@debian.org>
2009-02-20 14:23:57 +0100

    Version 0.07.

--- lib/B/Hooks/EndOfScope.pm
+++ lib/B/Hooks/EndOfScope.pm
@@ -6,7 +6,7 @@ package B::Hooks::EndOfScope;
 use 5.008000;
 use Variable::Magic;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Sub::Exporter -setup => {
     exports => ['on_scope_end'],
