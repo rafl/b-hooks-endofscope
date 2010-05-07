06b459fbc7934bd8267e6a9d0e1856c8bf3d0bf3
Florian Ragwitz <rafl@debian.org>
2010-05-07 22:11:41 +0200

    Port to dzil and @FLORA.

--- lib/B/Hooks/EndOfScope.pm
+++ lib/B/Hooks/EndOfScope.pm
@@ -2,20 +2,16 @@ use strict;
 use warnings;
 
 package B::Hooks::EndOfScope;
+# ABSTRACT: Execute code after a scope finished compilation
 
 use 5.008000;
-use Variable::Magic;
-
-our $VERSION = '0.08';
+use Variable::Magic 0.34;
 
 use Sub::Exporter -setup => {
     exports => ['on_scope_end'],
     groups  => { default => ['on_scope_end'] },
 };
 
-=head1 NAME
-
-B::Hooks::EndOfScope - Execute code after a scope finished compilation
 
 =head1 SYNOPSIS
 
@@ -26,9 +22,7 @@ B::Hooks::EndOfScope - Execute code after a scope finished compilation
 This module allows you to execute code when perl finished compiling the
 surrounding scope.
 
-=head1 FUNCTIONS
-
-=head2 on_scope_end
+=func on_scope_end
 
     on_scope_end { ... };
 
@@ -65,18 +59,6 @@ This is exported by default. See L<Sub::Exporter> on how to customize it.
 L<Sub::Exporter>
 
 L<Variable::Magic>
-
-=head1 AUTHOR
-
-Florian Ragwitz E<lt>rafl@debian.orgE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (c) 2008  Florian Ragwitz
-
-This module is free software.
-
-You may distribute this code under the same terms as Perl itself.
 
 =cut
 
